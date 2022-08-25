bdf77bb6a093142473183e895a214f2f7b0fdd1d
ramen4lyfe <do.quan.m@gmail.com>
2022-08-24 23:35:35 -0700

    add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import EventContext from './components/context/EventContext';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import CreateModal from './components/modals/CreateModal';
+import EventList from './components/EventList';
+import UpdateModal from './components/modals/UpdateModal';
 
 function App() {
   return (
-    <div className="App">
-
-      <div>Your mom</div>
-
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <BrowserRouter>
+            <Routes>
+              {/* <Route path="/register" element={<Register/>} />
+              <Route path="/login" element={<Login/>} /> */}
+              <Route path="/schedule/create" element={<CreateModal />} /> 
+              <Route path="/schedule/update/:id" element={<UpdateModal/>} />
+              <Route path="/schedule/view" element={<EventList />} />
+              {/* <Route path="/" element={<WeekView />} />
+              <Route path="/" element={<DayView />} /> */}
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
     </div>
   );
 }
