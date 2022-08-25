0337642fea190e95a098623afdbace062c585864
ramen4lyfe <do.quan.m@gmail.com>
2022-08-24 21:55:16 -0700

    added routes

--- server/routes/Event.routes.js
+++ server/routes/Event.routes.js
@@ -1,1 +1,9 @@
-const Event = require('../controllers/Event.controller')
+const EventController = require('../controllers/Event.controller')
+
+module.exports = (app) => {
+    app.get('/schedule', EventController.getEvent)
+    app.get('/schedule/:id', EventController.getEventById)
+    app.post('/schedule', EventController.createEvent)
+    app.put('/schedule/:id', EventController.updateEvent)
+    app.delete('/schedule/:id', EventController.deleteEvent)
+};
