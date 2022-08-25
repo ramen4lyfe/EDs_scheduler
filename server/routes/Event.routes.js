e290530d20c0c5ae89c510280f0e0b9a0f71094b
ramen4lyfe <do.quan.m@gmail.com>
2022-08-24 22:16:46 -0700

    updated routes

--- server/routes/Event.routes.js
+++ server/routes/Event.routes.js
@@ -1,9 +1,9 @@
-const EventController = require('../controllers/Event.controller')
+const eventController = require('../controllers/Event.controller');
 
 module.exports = (app) => {
-    app.get('/schedule', EventController.getEvent)
-    app.get('/schedule/:id', EventController.getEventById)
-    app.post('/schedule', EventController.createEvent)
-    app.put('/schedule/:id', EventController.updateEvent)
-    app.delete('/schedule/:id', EventController.deleteEvent)
+    app.get('/schedule', eventController.getEvent)
+    app.get('/schedule/:id', eventController.getEventById)
+    app.post('/schedule', eventController.createEvent)
+    app.put('/schedule/:id', eventController.updateEvent)
+    app.delete('/schedule/:id', eventController.deleteEvent)
 };
