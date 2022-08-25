46a6dc3b6bf8a6809429f7e084920261b2bf781c
ramen4lyfe <do.quan.m@gmail.com>
2022-08-24 21:32:23 -0700

    setup schema

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const dbName= "eds_schedule";
+ 
+mongoose
+.connect(`mongodb://localhost/${dbName}`,{
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+})
+   .then(() => console.log(`Connected to ${dbName} database!`))
+   .catch((err) => console.log(err));
