ce7ae33e46db48fd7d4fe8f7b869119e0c4ed89a
ramen4lyfe <do.quan.m@gmail.com>
2022-08-24 22:40:05 -0700

    attempted context

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import EventContext from './components/context/EventContext';
 
 
 function App() {
   return (
     <div className="App">
+
       <div>Your mom</div>
+
     </div>
   );
 }
