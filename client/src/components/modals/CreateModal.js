b8c081abe74bbd237a3a936b585c61f1e0d4b092
ramen4lyfe <do.quan.m@gmail.com>
2022-08-27 11:50:53 -0700

    cleaning up CreateModal

--- client/src/components/modals/CreateModal.js
+++ client/src/components/modals/CreateModal.js
@@ -1,67 +1,14 @@
 import {React, useState, useEffect} from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import CreateForm from '../../forms/CreateForm'; 
 
 const CreateModal = () => {
-  // const [gameName, setGameName] = useState("")
-  // const [date, setDate] = useState("")
-  // const [startTime, setStartTime] = useState("")
-  // // const [endTime, setEndTime] = useState("")
-  // const [hostEmployee, setHostEmployee] = useState("")
-  // const [gmEmployee, setGmEmployee] = useState("")
-
-  // const [userList, setUserList] = useState([])
-
-  // const [errors, setErrors] = useState("");
-  // const navigate = useNavigate();
-
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-//   useEffect(() =>{
-//     axios.get("http://localhost:8000/api/user/")
-//     .then((res=>{
-//         console.log(res);
-//         console.log(res.data);
-//         setUserList(res.data);
-//     }))
-//     .catch((err)=>console.log(err))
-// }, [])
-
-  // const handleSubmit = (e) => {
-  //   // e.preventDefault()
-  //   axios.post("http://localhost:8000/schedule", {
-  //     gameName, 
-  //     date, 
-  //     startTime, 
-  //     // endTime, 
-  //     hostEmployee, 
-  //     gmEmployee})
-  //   .then(response => {
-  //     console.log('Event Created Succesfully', response);
-  //     setGameName("");
-  //     setDate("");
-  //     setStartTime("");
-  //     // setEndTime("");
-  //     setHostEmployee("");
-  //     setGmEmployee("");
-  //     navigate("/schedule/view");
-  //     setShow(false);
-  //     // window.location.reload(false)
-  // })
-  // .catch((err) => {
-  //     console.log(err)
-  //     setErrors(err.response.data.error.errors);
-  // });
-  // }
-
 
   return (
     <>
