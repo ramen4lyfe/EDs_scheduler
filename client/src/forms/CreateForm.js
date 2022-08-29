15cb7e6a295c9d2d5923a14444b616078f8045a6
ramen4lyfe <do.quan.m@gmail.com>
2022-08-28 23:10:32 -0700

    updates

--- client/src/forms/CreateForm.js
+++ client/src/forms/CreateForm.js
@@ -44,30 +44,11 @@ const CreateForm = () => {
     }
 
     const [value, setValue] = useState([1, 3]);
-
-    /*
-    * The second argument that will be passed to
-    * `handleChange` from `ToggleButtonGroup`
-    * is the SyntheticEvent object, but we are
-    * not using it in this example so we will omit it.
-    */
     const handleChange = (val) => setValue(val);
     
 
 return (
     <div>
-    <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
-        <ToggleButton id="tbg-btn-1" value={1}>
-        Option 1
-        </ToggleButton>
-        <ToggleButton id="tbg-btn-2" value={2}>
-        Option 2
-        </ToggleButton>
-        <ToggleButton id="tbg-btn-3" value={3}>
-        Option 3
-        </ToggleButton>
-    </ToggleButtonGroup>
-    
 
     <InputGroup className="mb-3" controlId="gameName">
         <InputGroup.Text id="Game">Game</InputGroup.Text>
