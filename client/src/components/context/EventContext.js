37de238fb3dc2cd7f6f8d1808c7d89c4f937cb7c
ramen4lyfe <do.quan.m@gmail.com>
2022-08-29 17:11:35 -0700

    creating context

---
+++ client/src/components/context/EventContext.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { useEffect, useState, createContext } from 'react';
+
+export const EventContext = createEvent()
+
+
+const EventContextProvider = (props) => {
+    const[event, setEvent] = useState([
+    axios.get("http://localhost:8000/schedule")
+    .then((res=>{
+        console.log(res);
+        console.log(res.data);
+        setEventList(res.data);            
+    }))
+    .catch((err)=>console.log(err))
+    ])
+
+    return (
+        <EventContext.Provider value={{event}}>
+            {props.children}
+        </EventContext.Provider>
+    )
+}
+
+export default EventContextProvider;
