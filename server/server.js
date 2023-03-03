831edce4d05ca38f8e4fbf6208393761d9783c8a
ramen4lyfe <do.quan.m@gmail.com>
2023-03-03 02:35:36 -0800

    change port

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require("./config/mongoose.config");
 require("./routes/event.routes")(app);
-app.listen(8000, () => {
- console.log("Listening at Port 8000");
+app.listen(3000, () => {
+ console.log("Listening at Port 3000");
 });
