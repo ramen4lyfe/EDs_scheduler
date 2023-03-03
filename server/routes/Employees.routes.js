1706a961137e55f975f86c8ba405873f7d338398
ramen4lyfe <do.quan.m@gmail.com>
2023-03-03 01:31:34 -0800

    adding comments

--- server/routes/Employees.routes.js
+++ server/routes/Employees.routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const employeeController = require('../controllers/employeeController');
 
 module.exports = (app) => {
-app.get('/employee/:id', employeeController.getEmployeeById);
-app.get('/employee', employeeController.getEmployees);
-app.post('/employee', employeeController.createEmployee);
-app.patch('/employee/:id', employeeController.updateEmployee);
-app.delete('/employee/:id', employeeController.deleteEmployee);
+    app.get('/employee', employeeController.getEmployees); // Retrieve all emppoloyee from DB
+    app.get('/employee/:id', employeeController.getEmployeeById); // Retrieves a single employee by ID from DB
+    app.post('/employee', employeeController.createEmployee); // Creates a new employee in DB
+    app.patch('/employee/:id', employeeController.updateEmployee); // Updates existing employee by ID in DB
+    app.delete('/employee/:id', employeeController.deleteEmployee); //Deletes an employee by ID from DB
 };
